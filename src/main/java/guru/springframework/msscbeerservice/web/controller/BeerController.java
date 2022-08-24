063034946976a2ecbd3383086ec6e2b1c64447aa
mihai <mihai.iusan@gmail.com>
2022-08-24 16:47:04 +0200

    Add error handler. Closes #9

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbeerservice.web.controller;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -18,13 +19,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity<HttpStatus> saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
         //todo impl
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity<HttpStatus> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity<HttpStatus> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
         //todo impl
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
